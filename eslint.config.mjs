c4445c8152f2739eab7cf72a9e78044c34cc00a2
Sunita Prajapati <sunita.kuamari@daffodilsw.com>
2026-07-27 11:56:27 +0530

    chore: configure ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: [
+      "**/dist/**",
+      "**/node_modules/**",
+      "**/.yarn/**",
+      "**/coverage/**",
+      "**/*.d.ts",
+    ],
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+
+    languageOptions: {
+      parser: tseslint.parser,
+
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+
+      globals: {
+        ...globals.node,
+      },
+    },
+
+    rules: {
+      "no-console": "warn",
+      "no-debugger": "error",
+    },
+  },
+];
